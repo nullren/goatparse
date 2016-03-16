7efeff8d5dc3b0951f6726223786d2b5d6515c5b
Renning Bruns <chopflop@gmail.com>
2016-03-15 20:27:59 -0700

    gofmt

--- goatparse_test.go
+++ goatparse_test.go
@@ -8,7 +8,7 @@ import (
 func TestSimpleParse(t *testing.T) {
 	input := "3 minutes - 4 days that i need to go to the store"
 	result, _ := ParseDuration(input)
-	expected := 3 * time.Minute - 4 * time.Hour * 24
+	expected := 3*time.Minute - 4*24*time.Hour
 	if result.Duration != expected {
 		t.Error("expected ", expected, " but received ", result)
 	}
